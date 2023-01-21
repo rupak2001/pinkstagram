ec9b9e76120e26e359f21480439697caa4fb820c
Rupak Biswas <rupak.rb2001@gmail.com>
2023-01-21 15:51:47 +0530

    minor changes

--- ig/src/utils/url.js
+++ ig/src/utils/url.js
@@ -1,4 +1,4 @@
-const serverURL = "https://pinkstagram-server.herokuapp.com";
-// const serverURL = `http://localhost:${process.env.PORT || 8000}`;
+// const serverURL = "https://pinkstagram-server.herokuapp.com";
+const serverURL = `http://localhost:${process.env.PORT || 8000}`;
 
 export default serverURL;
